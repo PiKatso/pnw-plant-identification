b039b9c8d9bedece8fdaf33773d37c784121ced0
Kat Gifford <katgiff2007@gmail.com>
2017-07-24 11:57:07 -0700

    add preliminary scopes

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,12 @@
 class Plant < ApplicationRecord
   validates :name, :scientific_name, :plant_type, :general, :ecology, :leaves, :cones, :bark, :branches, presence: true
 
+  # Scopes
+  scope :s_b_leaves, -> (leaves) { where leaves: leaves }
+  scope :s_b_cones, -> (cones) { where cones: cones }
+  scope :s_b_bark, -> (bark) { where bark: bark }
+  scope :s_b_branches, -> (branches) { where branches: branches }
+
+  # Scope for Name Likeness
+  scope :s_b_name, -> (name) { where("name like ?", "#{name}%")}
 end
