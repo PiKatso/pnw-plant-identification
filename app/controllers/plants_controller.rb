408b997a27a14c4f8064dd41d8c1a3e3f47317a3
Kat Gifford <katgiff2007@gmail.com>
2017-07-26 10:38:33 -0700

    beg refactor scope to have dynamic elimination

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -9,18 +9,18 @@ class PlantsController < ApplicationController
     # # creates an anonymous scope
     @plants = Plant.where(nil)
 
-    @plants = @plants.search_all(@@leaves, @@cones, @@bark, params[:branches]) if params.values_at( :branches).all?(&:present?)
-    
+    # @plants = @plants.search_all(@@leaves, @@cones, @@bark, params[:branches]) if params.values_at( :branches).all?(&:present?)
+
     @@leaves = params[:leaves] if params[:leaves].present?
     @@cones = params[:cones] if params[:cones].present?
     @@bark = params[:bark] if params[:bark].present?
     @@branches = params[:branches] if params[:branches].present?
 
     # single search params SCOPES
-    # @plants = @plants.s_b_leaves(params[:leaves]) if params[:leaves].present?
-    # @plants = @plants.s_b_cones(params[:cones]) if params[:cones].present?
-    # @plants = @plants.s_b_bark(params[:bark]) if params[:bark].present?
-    # @plants = @plants.s_b_branches(params[:branches]) if params[:branches].present?
+    @@plants = @plants.s_b_leaves(params[:leaves]) if params[:leaves].present?
+    @@plants = @@plants.s_b_cones(params[:cones]) if params[:cones].present?
+    @@plants = @@plants.s_b_bark(params[:bark]) if params[:bark].present?
+    @plants = @@plants.s_b_branches(params[:branches]) if params[:branches].present?
 
     # name scope
     @plants = @plants.s_b_name(params[:name]) if params[:name].present?
