95138c7be56f098090fa9a1331b487e4fdd0f3e9
Kat Gifford <katgiff2007@gmail.com>
2017-07-27 10:01:13 -0700

    update controller/commit out unused crud pages for now

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -44,43 +44,43 @@ class PlantsController < ApplicationController
     @plant = Plant.find(params[:id])
   end
 
-  def new
-    @plant = Plant.new
-  end
-
-  def create
-    @plant = Plant.new(plant_params)
-    if @plant.save
-      flash[:notice] = "Plant successfully added"
-      redirect_to Plants_path
-    else
-      render :new
-      flash[:notice] = "Plant creation failed"
-    end
-  end
-
-  def edit
-    @plant = Plant.find(params[:id])
-  end
-
-  def update
-    @plant = Plant.find(params[:id])
-    if @plant.update(plant_params)
-      flash[:notice] = "Plant successfully updated"
-      redirect_to plant_path(@plant)
-    else
-      flash[:notice] = "Plant update failed"
-      render :edit
-    end
-  end
-
-  def destroy
-    @plant = Plant.find(params[:id])
-    if @plant.destroy
-      flash[:notice] = "Plant successfully removed"
-      redirect_to Plants_path
-    end
-  end
+  # def new
+  #   @plant = Plant.new
+  # end
+  #
+  # def create
+  #   @plant = Plant.new(plant_params)
+  #   if @plant.save
+  #     flash[:notice] = "Plant successfully added"
+  #     redirect_to Plants_path
+  #   else
+  #     render :new
+  #     flash[:notice] = "Plant creation failed"
+  #   end
+  # end
+  #
+  # def edit
+  #   @plant = Plant.find(params[:id])
+  # end
+  #
+  # def update
+  #   @plant = Plant.find(params[:id])
+  #   if @plant.update(plant_params)
+  #     flash[:notice] = "Plant successfully updated"
+  #     redirect_to plant_path(@plant)
+  #   else
+  #     flash[:notice] = "Plant update failed"
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @plant = Plant.find(params[:id])
+  #   if @plant.destroy
+  #     flash[:notice] = "Plant successfully removed"
+  #     redirect_to Plants_path
+  #   end
+  # end
 
 private
 
